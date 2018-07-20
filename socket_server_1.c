7417766e0ea789e269ecf02f02541c8d95cac21b
xuxiangfengshiwo <xuxiangfengshiwo@163.com>
2018-07-19 20:03:45 -0400

    add sock_sync_send

--- socket_server_1.c
+++ socket_server_1.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <fcntl.h>
+
 /***************define   ********/
 
 #define MAX_CONN   5
@@ -22,9 +24,14 @@ pid_t app_id;
 typedef int  SockRecvfd;
 bool sock_create(Sock_Channel_T * channel);
 
+/*  socket server  side  reply &&  recv   */
 bool sock_sync_recv(Sock_Channel_T * server_channel, void * buf, size_t buf_len, SockRecvfd * client);
 
 bool sock_sync_reply(SockRecvfd  client, const void * buf, size_t buf_len);
+
+/*  socket client side send  &&  recv  */
+
+bool sock_sync_send(Sock_Channel_T * server_channel, void * buf, size_t buf_len);
 /***********  create unix socket server***********/
 /*************
 @para: channel
@@ -41,7 +48,7 @@ bool sock_create(Sock_Channel_T *  channel)
 	channel->app_id = pid;
         /* create unix sock by process id*/
 	struct sockaddr_un addr;
-	snprintf(addr.sun_path, sizeof(addr.sun_path),"/tmp/sock_%d",(int)pid);
+	snprintf(addr.sun_path, sizeof(addr.sun_path),"/tmp/sock_%d",(int)(channel->app_id));
 	addr.sun_family = AF_LOCAL;
 	
         if(channel->fd>=0)
@@ -78,7 +85,7 @@ bool sock_sync_recv(Sock_Channel_T * server_channel, void * buf, size_t buf_len,
 	*client = accept(server_channel->fd, 0, 0);
 	if(*client != -1)
 	{
-		read(client, buf, buf_len);
+		read(*client, buf, buf_len);
 		status = true;
 	}
 	return status;
@@ -105,6 +112,22 @@ bool sock_sync_reply(SockRecvfd  client,const void * buf, size_t buf_len)
 	}
 	close(client);
 }
+
+bool sock_sync_send(Sock_Channel_T * server_channel, void * buf, size_t buf_len)
+{
+	SockRecvfd client;
+        struct sockaddr_un addr;
+		
+	client = socket(AF_UNIX, O_CLOEXEC|SOCK_STREAM, 0 );
+	 /* connect unix sock by process id*/
+      
+        snprintf(addr.sun_path, sizeof(addr.sun_path),"/tmp/sock_%d",(int)(server_channel->app_id));
+        addr.sun_family = AF_LOCAL;
+	
+	connect(client, (struct sockaddr *)(void *)&addr, sizeof(addr));
+	
+}
+
 int main(int argc, char **argv)
 {
      	char read_buf[1024];
